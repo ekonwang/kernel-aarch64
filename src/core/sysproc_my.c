bb6d7c2b269f7103e86e8d8a2d41aa14f68c02d5
Yikun <g1547246193@gmail.com>
2021-11-01 11:46:43 +0800

    merge lab4 - sysproc.c

--- src/core/sysproc_my.c
+++ src/core/sysproc_my.c
@@ -1,13 +1,13 @@
-#include <core/syscall.h>
 #include <core/console.h>
 #include <core/proc.h>
+#include <core/syscall.h>
 
 void sys_myexecve(char *s) {
     printf("sys_exec: executing %s\n", s);
     return;
 }
 
-NO_RETURN void sys_myexit() {
+void sys_myexit() {
     printf("sys_exit: in exit\n");
     exit();
 }
